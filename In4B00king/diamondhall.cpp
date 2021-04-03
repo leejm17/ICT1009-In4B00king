9a421c14f3f538a9dfe53139a2e80aeec92db53c
vizierofhope <limyongan8888@gmail.com>
2021-04-03 21:35:49 +0800

    Added "disabled" type

--- In4B00king/diamondhall.cpp
+++ In4B00king/diamondhall.cpp
@@ -19,7 +19,7 @@ DiamondHall::~DiamondHall()
 void DiamondHall::updateSeats()
 {
     QSqlQuery query(MyDB::getInstance()->getDBInstance());
-    query.prepare("select HallSeats.seat_num,MovieSeats.available,HallSeats.condition from HallSeats inner join MovieSeats on HallSeats.seat_ID=MovieSeats.seat_ID where MovieSeats.show_ID=1");
+    query.prepare("select HallSeats.seat_num,MovieSeats.available,HallSeats.condition,HallSeats.type from HallSeats inner join MovieSeats on HallSeats.seat_ID=MovieSeats.seat_ID where MovieSeats.show_ID=1");
 
     if(!query.exec())
     {
@@ -32,10 +32,12 @@ void DiamondHall::updateSeats()
         QLabel *sptr = this->findChild<QLabel*>(sLabel);
         if(sptr!=nullptr)
         {
-            if(query.value(1).toString().compare("TRUE") == 0 && query.value(2).toString().compare("good") == 0)
+            if(query.value(1).toString().compare("TRUE") == 0 && query.value(2).toString().compare("good") == 0 && query.value(3).toString().compare("disabled") == 0)
             {
+                sptr->setStyleSheet("QLabel { background-color : blue; }");
+            }else if(query.value(1).toString().compare("TRUE") == 0 && query.value(2).toString().compare("good") == 0){
                 sptr->setStyleSheet("QLabel { background-color : green; }");
-            } else {
+            }else {
                 sptr->setStyleSheet("QLabel { background-color : red; }");
             }
         }
