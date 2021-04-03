b60bb1e00ed2540cb02c8cec0e33e4c2775e0567
Yoobi <Yoobi@d.c>
2021-04-04 00:32:37 +0800

    db up

--- In4B00king/confirmationscreen.cpp
+++ In4B00king/confirmationscreen.cpp
@@ -23,7 +23,7 @@ void ConfirmationScreen::showConfirmation(QString seat)
 void ConfirmationScreen::on_confirm_clicked()
 {
     QSqlQuery query(MyDB::getInstance()->getDBInstance());
-    query.prepare("update MovieSeats set MovieSeats.available='FALSE' where exists (select null from HallSeats inner join MovieSeats on HallSeats.seat_ID=MovieSeats.seat_ID where seat_num='"+ui->seat->text()+"' and MovieSeats.show_ID=1 and MovieSeats.available='TRUE' and HallSeats.condition='good')");
+    query.prepare("update MovieSeats set available='FALSE' where seat_ID=(select MovieSeats.seat_ID from MovieSeats inner join HallSeats on MovieSeats.seat_ID=HallSeats.seat_ID where HallSeats.seat_num='A1' and MovieSeats.show_ID=1 and MovieSeats.available='TRUE' and HallSeats.condition='good')");
 
     if(!query.exec())
     {
