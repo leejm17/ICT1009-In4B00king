ecfdfe4f8e575bee635724ec3553e0c2674dd9f5
Yoobi <Yoobi@a.c>
2021-04-07 11:02:06 +0800

    for YALIM

--- In4B00king/movieinformation.cpp
+++ In4B00king/movieinformation.cpp
@@ -44,23 +44,25 @@ void MovieInformation::on_Back_clicked()
 void MovieInformation::on_SelectSeats_clicked()
 {
     QString selection = ui->DropdownDate->currentText();
-    QString selectedTime = selection.split(" ").at(1);
-    QString selectedDate = selection.split(" ").at(0);
-
-    qDebug() << this->MovieID;
+    QString selectedTime = selection.split(" ").at(2);
+    QString selectedDate = selection.split(" ").at(1);
+    QString selectedHall = selection.split(" ").at(0);
 
     QSqlQuery query(MyDB::getInstance()->getDBInstance());
-    query.prepare("select MovieShowing.show_ID from Halls inner join MovieShowing on Halls.hall_ID=MovieShowing.hall_ID where MovieShowing.movie_ID=" + QString::number(this->MovieID) +" and MovieShowing.date='" + selectedDate + "' and MovieShowing.timeslot='" + selectedTime + "' and Halls.type='economy'");
+    query.prepare("select MovieShowing.show_ID,Halls.type from Halls inner join MovieShowing on Halls.hall_ID=MovieShowing.hall_ID where MovieShowing.movie_ID=" + QString::number(this->MovieID) +" and MovieShowing.date='" + selectedDate + "' and MovieShowing.timeslot='" + selectedTime + "' and MovieShowing.hall_ID='"+ selectedHall+"'");
 
     if(!query.exec())
     {
         qDebug() << query.lastError().text() << query.lastQuery();
         QMessageBox::warning(this,"Error",query.lastError().text());
     }
-    query.next();
-            /*if( == 0){
+    while(query.next())
+    {
+    qDebug() << query.value(1).toString();
+            if(query.value(1).toString().compare("economy") == 0){
             emit updateESeats(selectedTime, selectedDate, query.value(0).toInt());
             } else {
                 emit updateDSeats(selectedTime, selectedDate, query.value(0).toInt());
-            }*/
+            }
+    }
 }
